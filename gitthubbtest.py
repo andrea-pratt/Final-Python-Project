2de073f4c3838e0cb24cda15a232854e24258b79
Your Name <you@example.com>
2019-12-04 10:22:44 -0600

    Added line 3

--- gitthubbtest.py
+++ gitthubbtest.py
@@ -1,4 +1,4 @@
 print('This is my initial github code.')
 print('Now I will bring this down into Pycharm')
 # Now I'll send this back up to GitHub
-dflgjdfgkldfjglkdfjgdff
+# That red was annoying me
