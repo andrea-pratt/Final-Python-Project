146851314d4f932ce9802cd4a4bce9c0b5624355
Your Name <you@example.com>
2019-12-04 13:21:54 -0600

    Attempt to begin ITEC schedule program

--- Final_Python_Project.py
+++ Final_Python_Project.py
@@ -1,1 +1,12 @@
-# Initial GitHub test
+# Importing beautiful soup module
+import requests
+import bs4
+res = requests.get("""https://eservices.minnstate.edu/registration/search/advancedSubmit.html?campusid=305&searchrcid
+=0305&searchcampusid=305&yrtr=20205&subject=ITEC&courseNumber=&courseId=&openValue=OPEN_PLUS_WAITLIST&delivery=ALL&
+showAdvanced=&starttime=&endtime=&mntransfer=&credittype=ALL&credits=&instructor=&keyword=&begindate=&site=&
+resultNumber=250
+""")
+res.raise_for_status()
+noStarchSoup = bs4.BeautifulSoup(res.text)
+type(noStarchSoup)
+print(noStarchSoup)
