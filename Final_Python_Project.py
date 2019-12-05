7d79553d5172622323f15a2c7927d9cbb3b1e262
Your Name <you@example.com>
2019-12-04 18:34:42 -0600

    Fixed some of the json referencing.

--- Final_Python_Project.py
+++ Final_Python_Project.py
@@ -2,15 +2,10 @@ import requests
 
 for number in range(1):
     data = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()
-    base_layer_name = data['base_layer']['name']
     base_layer_recipe = data['base_layer']['recipe']
-    seasoning_name = data['seasoning']['name']
     seasoning_recipe = data['seasoning']['recipe']
-    mixin_name = data['mixin']['name']
     mixin_recipe = data['mixin']['recipe']
-    condiment_name = data['condiment']['name']
-    condiment_recipe = data['condiment']['name']
-    shell_name = data['shell']['name']
-    shell_recipe = data['shell']['name']
+    condiment_recipe = data['condiment']['recipe']
+    shell_recipe = data['shell']['recipe']
     print(f'{base_layer_recipe}\n{seasoning_recipe}\n{mixin_recipe}\n')
     print(f'{condiment_recipe}\n{shell_recipe}')
