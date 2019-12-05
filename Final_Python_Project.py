9aaf24221b75df1ffcec45c3b06a920e8dbd209e
Your Name <you@example.com>
2019-12-04 19:08:06 -0600

    Created a simplified version of recipe book word doc.

--- Final_Python_Project.py
+++ Final_Python_Project.py
@@ -1,6 +1,9 @@
 import requests
+import docx
 
 # TODO add try/except block and add code to create word document out of this data
+document = docx.Document()
+
 for number in range(3):
     data = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()
     base_layer_recipe = data['base_layer']['recipe']
@@ -8,5 +11,6 @@ for number in range(3):
     mixin_recipe = data['mixin']['recipe']
     condiment_recipe = data['condiment']['recipe']
     shell_recipe = data['shell']['recipe']
-    print(f'{base_layer_recipe}\n{seasoning_recipe}\n{mixin_recipe}\n')
-    print(f'{condiment_recipe}\n{shell_recipe}')
+    document.add_paragraph(f'Recipe Number {number + 1}')
+    document.add_paragraph(f'{base_layer_recipe}\n\n\n{seasoning_recipe}\n\n\n{mixin_recipe}\n\n\n{condiment_recipe}\n\n\n{shell_recipe}')
+    document.save('random_taco_recipes.docx')
